48a2666df0560ce30f22b81b7015ecce5bc453a8
João Pedro Fonseca <joaopfon2020@gmail.com>
2025-05-09 12:39:13 -0300

    Adicionando script.js com novo código

---
+++ jcripts.js
@@ -0,0 +1,19 @@
+console.log(function toggleMode() {git add script.js
+  const html = document.documentElement
+  html.classList.toggle(light)
+
+  // pegar a tag da imagem
+  const img = document.querySelector(#profile img)
+
+  // substituir a imagem
+  if (html.classList.contains(light)) {
+    // se tiver light mode adicionar a imagem light
+
+    img.setAttribute(src, ./assets/avatar-light.png)
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+
+    img.setAttribute(src, ./assets/avatar-dark1.5.png)
+  }
+});
+
